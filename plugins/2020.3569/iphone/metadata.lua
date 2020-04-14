3cbcfab59f72d5a60a56b86e00014e4090c58b21
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:54:21 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDChartboostAdapter', },
+		frameworks = { 'Chartboost', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
